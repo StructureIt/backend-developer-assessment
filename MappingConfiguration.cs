2da1e1c14601850b6251371d3965be579eb636f0
Navamohan <navamohank@gmail.com>
2016-12-07 18:32:17 +1300

    Views configuration and html files with solution
    Hope fully the downloaded  files compile at your end, can submit a published version to host in an iis can be given next release. Please review the changes and give your feedback.

---
+++ MappingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using AutoMapper;
+using SearchApiService.AutoMapper;
+
+namespace SearchApiService
+{
+    public static class MappingConfiguration
+    {
+        public static void RegisterMappings()
+        {
+            Debug.WriteLine("RegisterMappings Start: {0}", DateTime.Now.ToLongTimeString());
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DtoToDomain>();
+            });
+            Debug.WriteLine("RegisterMappings End: {0}", DateTime.Now.ToLongTimeString());
+        }
+    }
+}
